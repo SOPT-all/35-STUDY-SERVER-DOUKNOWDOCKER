5cecb5dcc07246a956ce9d0292e31a00f4771c97
정윤 <sju08227@naver.com>
2024-11-13 17:33:50 +0900

    Delete umc4th/carrotMarket/src/main/java/com/sopt/carrotMarket directory

--- umc4th/carrotMarket/src/main/java/com/sopt/carrotMarket/ApiUtils.java
+++
@@ -1,9 +0,0 @@
-package com.sopt.carrotMarket;
-
-import org.springframework.http.HttpStatus;
-
-public class ApiUtils {
-    public static <T> CommonResponse<T> success(int code, HttpStatus httpStatus, T result) {
-        return new CommonResponse<>(code, httpStatus, result);
-    }
-}
